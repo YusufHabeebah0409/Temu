a06a85d476f67ba1769e30327dc823f283ecab4d
YusufHabeebah0409 <ojuolapehabeebah@gmail.com>
2025-04-17 15:20:09 +0100

    Refactor item addition and display logic; enhance error handling and delete button visibility

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const cart = []
+
 const addItem = () => {
     if (item.value === "") {
-        alert("Input something joor")
+        // alert("Input something joor")
+        errorMsg.style.display = 'block'
     } else {
+        errorMsg.style.display = 'none'
         cart.push(item.value)
-        document.getElementById('item').value =''
+        document.getElementById('item').value = ''
         console.log(cart);
-        
+        if (cart.length >= 1) {
+            btnDelete.style.display = 'block'
+        }
+
         show.innerHTML = ''
-        for(let i=0;i<cart.length;i++){
-           show.innerHTML += `
-            <p>${i+1}.${cart[i]}</p>
+        for (let i = 0; i < cart.length; i++) {
+            show.innerHTML += `
+            <p>${i + 1}.${cart[i]}</p>
            `
         }
     }
 
 }
+
+const deleteAnyItem = () => {
+    7
+}
+
